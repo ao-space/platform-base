9b252626e423087292f89dd39730c03cd3941bec
陶冶 <taoye@iscas.ac.cn>
2021-08-26 10:25:09 +0800

    fix sonar issues

--- src/main/java/xyz/eulix/platform/services/notify/dto/NotifySessionInfo.java
+++ src/main/java/xyz/eulix/platform/services/notify/dto/NotifySessionInfo.java
@@ -11,9 +11,9 @@ public class NotifySessionInfo {
     @Getter
     private final Session session;
     ///登录超时时间，超过这个时间，则自动断开连接
-    private final long loginExpiredIntervalMillis = 5 * 1000;
+    private final long loginExpiredIntervalMillis = 5 * 1000L;
     ///心跳超时时间，超过这个时间，则自动断开连接
-    private final long heartbeatExpiredIntervalMillis = 60 * 1000;
+    private final long heartbeatExpiredIntervalMillis = 60 * 1000L;
     @Getter
     @Setter
     private String deviceId;
