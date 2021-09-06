64c52a740cfc2f0db243e411ca42bf640debce65
haibo <haibo_luo@icloud.com>
2021-09-06 12:57:26 +0800

    完善注册接口，删除目前不需要的定义和代码。

--- src/main/java/xyz/eulix/platform/services/registry/dto/registry/RegistryResetResult.java
+++ src/main/java/xyz/eulix/platform/services/registry/dto/registry/RegistryResetResult.java
@@ -1,6 +1,7 @@
 package xyz.eulix.platform.services.registry.dto.registry;
 
 import lombok.Data;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 /**
  * Used to define a data transfer object as REST response for holding related result
@@ -8,5 +9,6 @@ import lombok.Data;
  */
 @Data(staticConstructor = "of")
 public class RegistryResetResult {
+  @Schema(description = "盒子的 UUID")
   private final String boxUUID;
 }
