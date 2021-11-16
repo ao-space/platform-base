2cabe34efba3044ba404d54bf81586e518c4f7ae
郑祖岭 <zuling@iscas.ac.cn>
2021-11-16 08:56:04 +0800

    Task @158 | 修改“升级检查接口”

--- src/main/java/xyz/eulix/platform/services/mgtboard/dto/PackageCheckRes.java
+++ src/main/java/xyz/eulix/platform/services/mgtboard/dto/PackageCheckRes.java
@@ -13,11 +13,11 @@ public class PackageCheckRes {
 
     // app是否需要关联更新
     @Schema(description = "app是否需要关联更新")
-    private Boolean isAppNeedUpdate = false;
+    private Boolean isAppNeedUpdate;
 
     // box是否需要关联更新
     @Schema(description = "box是否需要关联更新")
-    private Boolean isBoxNeedUpdate = false;
+    private Boolean isBoxNeedUpdate;
 
     // 最新app软件包信息
     private PackageRes latestAppPkg;
