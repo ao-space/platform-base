9a7ebf21d1f95b4ab5801fdd838a79b9be2be801
zuling <zuling@iscas.ac.cn>
2021-11-26 10:48:36 +0800

    test数据库切换至mysql（mysql-ci-test）

--- src/main/resources/db/migration/R__Half_Completed.sql
+++ src/main/resources/db/migration/R__Half_Completed.sql
@@ -1,6 +1,7 @@
 -- 正式版本发布前临时脚本
 -- 判断 proposal 表是否存在 subdomain 字段，不存在则添加
 DELIMITER $
+DROP PROCEDURE IF EXISTS schema_change$
 CREATE PROCEDURE schema_change()
 BEGIN
 IF NOT EXISTS (SELECT * FROM information_schema.columns WHERE table_schema = DATABASE()  AND table_name = 'proposal' AND column_name = 'subdomain') THEN
