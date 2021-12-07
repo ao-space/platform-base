159a2eadacdc06815c0fd92aa47aac663cdf5c60
雒海波 <luohaibo@iscas.ac.cn>
2021-12-07 10:19:44 +0800

    Other | Fix err maps

--- src/main/java/xyz/eulix/platform/services/support/RestConfiguration.java
+++ src/main/java/xyz/eulix/platform/services/support/RestConfiguration.java
@@ -8,7 +8,7 @@ import org.jboss.logging.Logger;
 import xyz.eulix.platform.services.support.service.ServiceOperationException;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
@@ -35,6 +35,8 @@ public class RestConfiguration {
 
   static final Logger LOG = Logger.getLogger("rest.log");
 
+  public static final String REQUEST_ID = "Request-Id";
+
   /**
    * It provides exception result mapping for REST responses. For more information:
    * <a href="https://developer.jboss.org/docs/DOC-48310">https://developer.jboss.org/docs/DOC-48310</a>.
@@ -42,33 +44,58 @@ public class RestConfiguration {
   @Provider
   public static class ErrorMapper implements ExceptionMapper<Exception> {
 
+    @Context
+    HttpServerRequest request;
+
     @Override
     public Response toResponse(Exception exception) {
-      int status = Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
-      String message = exception.getMessage();
-      Integer code = null;
-      if (exception instanceof WebApplicationException) {
-        status = ((WebApplicationException) exception).getResponse().getStatus();
-      } else if (exception instanceof IllegalArgumentException) {
-        status = Response.Status.PRECONDITION_FAILED.getStatusCode();
-      } else if (exception instanceof ConstraintViolation) {
-        status = Response.Status.FORBIDDEN.getStatusCode();
-      } else if (exception instanceof ServiceOperationException) {
-        status = Response.Status.BAD_REQUEST.getStatusCode();
-        code = ((ServiceOperationException) exception).getErrorCode();
-      }
-
-      Map<String, Object> body = Maps.newLinkedHashMap();
-      body.put("exception", exception.getClass().getName());
-      if (code != null) {
-        body.put("code", code);
-      }
-      if (message != null) {
-        body.put("error", message);
-      }
-
-      return Response.status(status).entity(body).build();
+      return convertToErrorResponse(request, exception);
     }
+  }
+
+  /**
+   * 提供专门针对 ConstraintViolationException 异常的拦截，因为默认通过 {@link ErrorMapper} 无法拦截到这类异常。
+   * 具体参考：https://stackoverflow.com/questions/34452996/jaxrs-jersey-2-validation-errors-does-not-invoke-the-exceptionmapper
+   */
+  @Provider
+  public static class ConstraintViolationErrorMapper implements ExceptionMapper<ConstraintViolationException> {
+
+    @Context
+    HttpServerRequest request;
+
+    @Override
+    public Response toResponse(ConstraintViolationException exception) {
+      return convertToErrorResponse(request, exception);
+    }
+  }
+
+  private static Response convertToErrorResponse(HttpServerRequest request, Exception exception) {
+    String requestId = request.getHeader(REQUEST_ID);
+    requestId = ((requestId == null) ? request.getParam(REQUEST_ID) : requestId);
+    int status = Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
+    String message = exception.getMessage();
+    Integer code = null;
+    if (exception instanceof WebApplicationException) {
+      status = ((WebApplicationException) exception).getResponse().getStatus();
+    } else if (exception instanceof IllegalArgumentException) {
+      status = Response.Status.PRECONDITION_FAILED.getStatusCode();
+    } else if (exception instanceof ConstraintViolationException) {
+      status = Response.Status.BAD_REQUEST.getStatusCode();
+    } else if (exception instanceof ServiceOperationException) {
+      status = Response.Status.BAD_REQUEST.getStatusCode();
+      code = ((ServiceOperationException) exception).getErrorCode();
+    }
+
+    Map<String, Object> body = Maps.newLinkedHashMap();
+    body.put("requestId", requestId);
+    if (code != null) {
+      body.put("code", code);
+    }
+    if (message != null) {
+      body.put("error", message);
+    }
+
+    return Response.status(status).entity(body).build();
   }
 
   private final static String STOPWATCH = "stopwatch";
--- src/test/java/xyz/eulix/platform/services/mgtboard/rest/ProposalResourceTest.java
+++ src/test/java/xyz/eulix/platform/services/mgtboard/rest/ProposalResourceTest.java
@@ -1,12 +1,14 @@
 package xyz.eulix.platform.services.mgtboard.rest;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.keycloak.client.KeycloakTestClient;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+import xyz.eulix.platform.services.mgtboard.dto.ProposalReq;
 
 @QuarkusTest
 class ProposalResourceTest {
@@ -33,5 +35,17 @@ class ProposalResourceTest {
         .then()
         .statusCode(200);
 
+  }
+
+  @Test
+  void testProposalSaveRequestIdAbsentFailedOk() {
+    given().auth().oauth2(getAccessToken())
+        .contentType(ContentType.JSON)
+        .body(ProposalReq.of())
+        .when()
+        .post("/v1/api/proposal")
+        .then()
+        .statusCode(400)
+        .body(containsString("\"error\""));
   }
 }
