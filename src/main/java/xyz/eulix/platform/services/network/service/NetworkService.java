5c1c3aeb07efa9b2cee54078da4b7b3533c8bd0b
zuling <zuling@iscas.ac.cn>
2022-03-11 14:44:47 +0800

    platform支持获取 STUN 服务地址

--- src/main/java/xyz/eulix/platform/services/network/service/NetworkService.java
+++ src/main/java/xyz/eulix/platform/services/network/service/NetworkService.java
@@ -2,12 +2,20 @@ package xyz.eulix.platform.services.network.service;
 
 import org.jboss.logging.Logger;
 import xyz.eulix.platform.services.network.dto.NetworkAuthReq;
+import xyz.eulix.platform.services.network.dto.NetworkServerExtraInfo;
 import xyz.eulix.platform.services.network.dto.NetworkServerRes;
+import xyz.eulix.platform.services.network.dto.StunServerRes;
 import xyz.eulix.platform.services.network.entity.NetworkRouteEntity;
 import xyz.eulix.platform.services.network.entity.NetworkServerEntity;
 import xyz.eulix.platform.services.network.repository.NetworkRouteEntityRepository;
 import xyz.eulix.platform.services.network.repository.NetworkServerEntityRepository;
+import xyz.eulix.platform.services.registry.entity.RegistryBoxEntity;
+import xyz.eulix.platform.services.registry.entity.SubdomainEntity;
+import xyz.eulix.platform.services.registry.repository.RegistryBoxEntityRepository;
+import xyz.eulix.platform.services.registry.repository.SubdomainEntityRepository;
 import xyz.eulix.platform.services.registry.service.RegistryService;
+import xyz.eulix.platform.services.support.CommonUtils;
+import xyz.eulix.platform.services.support.serialization.OperationUtils;
 import xyz.eulix.platform.services.support.service.ServiceError;
 import xyz.eulix.platform.services.support.service.ServiceOperationException;
 
@@ -29,6 +37,15 @@ public class NetworkService {
 
     @Inject
     NetworkRouteEntityRepository routeEntityRepository;
+
+    @Inject
+    RegistryBoxEntityRepository registryBoxEntityRepository;
+
+    @Inject
+    SubdomainEntityRepository subdomainEntityRepository;
+
+    @Inject
+    OperationUtils operationUtils;
 
     /**
      * 计算network路由
@@ -81,11 +98,38 @@ public class NetworkService {
     }
 
     private NetworkServerRes networkServerEntityToRes(NetworkServerEntity serverEntity) {
-        return NetworkServerRes.of(serverEntity.getProtocol() + "://" + serverEntity.getAddr() + ":" + serverEntity.getPort());
+        return NetworkServerRes.of(serverEntity.getProtocol() + "://" + serverEntity.getAddr() + ":"
+                + serverEntity.getPort(), getExtraInfo(serverEntity).getStunAddress());
     }
 
     @Transactional
     public void deleteByClientID(String clientId) {
         routeEntityRepository.deleteByClientID(clientId);
+    }
+
+    public StunServerRes stunServerDetail(String subdomain) {
+        // 查询boxUUID
+        Optional<SubdomainEntity> subdomainEntityOp = subdomainEntityRepository.findBySubdomain(subdomain);
+        if (subdomainEntityOp.isEmpty()) {
+            LOG.warnv("subdomain does not exist, subdomain:{0}", subdomain);
+            throw new ServiceOperationException(ServiceError.SUBDOMAIN_NOT_EXIST);
+        }
+        // 查询盒子的network client信息
+        Optional<RegistryBoxEntity> registryBoxEntityOp = registryBoxEntityRepository.findByBoxUUID(subdomainEntityOp.get().getBoxUUID());
+        String networkClientId = registryBoxEntityOp.get().getNetworkClientId();
+        // 查询映射关系
+        Optional<NetworkRouteEntity> routeEntityOp = routeEntityRepository.findByClientId(networkClientId);
+        Long networkServerId = routeEntityOp.get().getServerId();
+        // 查询network server详情
+        NetworkServerEntity serverEntity = serverEntityRepository.findById(networkServerId);
+        return StunServerRes.of(getExtraInfo(serverEntity).getStunAddress());
+    }
+
+    private NetworkServerExtraInfo getExtraInfo(NetworkServerEntity serverEntity) {
+        if (CommonUtils.isNullOrEmpty(serverEntity.getExtra())) {
+            LOG.errorv("network server extra info is illegal");
+            return NetworkServerExtraInfo.of();
+        }
+        return operationUtils.jsonToObject(serverEntity.getExtra(), NetworkServerExtraInfo.class);
     }
 }
