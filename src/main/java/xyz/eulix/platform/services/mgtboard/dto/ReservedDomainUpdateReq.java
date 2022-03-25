cefca66e6ae149dff857a3990a30c8fcca09b902
仇文超 <wenchao@iscas.ac.cn>
2022-03-25 13:50:03 +0800

    保留域名的长度bug修复

--- src/main/java/xyz/eulix/platform/services/mgtboard/dto/ReservedDomainUpdateReq.java
+++ src/main/java/xyz/eulix/platform/services/mgtboard/dto/ReservedDomainUpdateReq.java
@@ -4,14 +4,17 @@ import lombok.Data;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data(staticConstructor = "of")
 public class ReservedDomainUpdateReq {
     @NotBlank
-    @Schema(required = true, description = "正则表达式")
+    @Size(max = 100,min = 1)
+    @Schema(required = true, description = "正则表达式", maxLength = 100, minLength = 1)
     private String regex;
 
     @NotBlank
-    @Schema(required = true, description = "规则描述")
+    @Size(max = 60)
+    @Schema(required = true, description = "规则描述", maxLength = 60)
     private String desc;
 }
