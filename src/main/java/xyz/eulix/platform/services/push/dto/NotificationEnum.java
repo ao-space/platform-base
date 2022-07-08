1d04156746f62abb2460e58d2a5bee2e1c802b82
郑祖岭 <zuling@iscas.ac.cn>
2022-07-08 16:02:22 +0800

    消息推送bug修复

--- src/main/java/xyz/eulix/platform/services/push/dto/NotificationEnum.java
+++ src/main/java/xyz/eulix/platform/services/push/dto/NotificationEnum.java
@@ -6,8 +6,8 @@ import xyz.eulix.platform.services.support.boundary.push.AfterOpenAction;
 
 @AllArgsConstructor
 public enum NotificationEnum {
-    BOX_UPGRADE("系统升级", "发现新的傲空间系统版本，点击查看", "系统升级推送", "box_upgrade", AfterOpenAction.GO_APP),
-    APP_UPGRADE("应用升级", "发现新的傲空间应用版本，点击查看", "应用升级推送", "app_upgrade", AfterOpenAction.GO_APP),
+    BOX_UPGRADE("系统升级", "发现新的傲空间系统版本，点击查看", "系统升级推送", "box_upgrade", AfterOpenAction.GO_ACTIVITY, "xyz.eulix.space.push.EulixMfrNotifyActivity"),
+    APP_UPGRADE("应用升级", "发现新的傲空间应用版本，点击查看", "应用升级推送", "app_upgrade", AfterOpenAction.GO_ACTIVITY, "xyz.eulix.space.push.EulixMfrNotifyActivity"),
     ;
 
     @Getter
@@ -20,6 +20,8 @@ public enum NotificationEnum {
     private final String type;
     @Getter
     private final AfterOpenAction afterOpenAction;
+    @Getter
+    private final String activity;
 
     public static NotificationEnum of(String type) {
         for (var notification : NotificationEnum.values()) {
