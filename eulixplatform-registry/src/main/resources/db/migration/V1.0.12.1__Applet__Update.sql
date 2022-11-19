b96424d2be824373013458b2cc089947cb0da62e
殷佳毅 <jiayi@iscas.ac.cn>
2022-11-19 17:07:46 +0000

    Other | Auto Fix License Header

--- eulixplatform-registry/src/main/resources/db/migration/V1.0.12.1__Applet__Update.sql
+++ eulixplatform-registry/src/main/resources/db/migration/V1.0.12.1__Applet__Update.sql
@@ -1,3 +1,17 @@
+-- Copyright (c) 2022 Institute of Software Chinese Academy of Sciences (ISCAS)
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 -- 正式版本发布前临时脚本
 ALTER TABLE applet_info ADD  web_down_url VARCHAR(256) COMMENT 'web程序下载地址';
 ALTER TABLE applet_info ADD  web_md5 VARCHAR(128) DEFAULT NULL;
