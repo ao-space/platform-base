59a269ac569ecef68b1ad07b51614300cb2b5f8d
Makonike <makonike@anyview.fun>
2023-08-05 00:39:33 +0800

    feat: add ut for AuthService

--- eulixplatform-registry/src/test/java/xyz/eulix/platform/services/auth/AuthServiceTest.java
+++ eulixplatform-registry/src/test/java/xyz/eulix/platform/services/auth/AuthServiceTest.java
@@ -16,14 +16,36 @@
 
 package xyz.eulix.platform.services.auth;
 
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import xyz.eulix.platform.services.auth.dto.GenPkeyRsp;
+import xyz.eulix.platform.services.auth.dto.PollPkeyRsp;
+import xyz.eulix.platform.services.auth.dto.TransBoxInfoReq;
+import xyz.eulix.platform.services.auth.repository.PkeyAuthEntityRepository;
 
+import javax.inject.Inject;
+import javax.transaction.Transactional;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static io.restassured.RestAssured.given;
+import static xyz.eulix.platform.common.support.service.ServiceError.PKEY_INVALID;
+
+@QuarkusTest
 public class AuthServiceTest {
+
+    @Inject
+    PkeyAuthEntityRepository pkeyAuthEntityRepository;
+
+    @AfterEach
+    @Transactional
+    void tearDown() {
+        pkeyAuthEntityRepository.delete("bkey", "b_key");
+    }
 
     @Test
     public void testUUID() {
@@ -32,5 +54,87 @@ public class AuthServiceTest {
         Pattern pattern = Pattern.compile( "[a-zA-Z0-9-]{36}");
         Matcher matcher = pattern.matcher(uuid);
         Assertions.assertTrue(matcher.matches());
+    }
+
+    @Test
+    public void genPkey() {
+        final String bid = UUID.randomUUID().toString();
+        final GenPkeyRsp result = given()
+                .header("Request-Id", bid)
+                .contentType(ContentType.JSON)
+                .when().post("/v2/platform/pkeys")
+                .body()
+                .as(GenPkeyRsp.class);
+
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    public void boxinfoTrans() {
+        final String bid = UUID.randomUUID().toString();
+        final GenPkeyRsp pkeyRsp = given()
+                .header("Request-Id", bid)
+                .contentType(ContentType.JSON)
+                .when().post("/v2/platform/pkeys")
+                .body()
+                .as(GenPkeyRsp.class);
+        Assertions.assertNotNull(pkeyRsp);
+
+        String pkey = pkeyRsp.getPkey();
+        TransBoxInfoReq info = new TransBoxInfoReq();
+        {
+            info.setBkey("b_key");
+            info.setUserDomain("user_domain");
+            info.setBoxPubKey("box_pub_key");
+        }
+        final PollPkeyRsp result = given()
+                .header("Request-Id", bid)
+                .body(info)
+                .contentType(ContentType.JSON)
+                .when().post(String.format("/v2/platform/pkeys/%s/boxinfo", pkey))
+                .body()
+                .as(PollPkeyRsp.class);
+        Assertions.assertNotNull(result);
+
+        final PollPkeyInvalidRsp invalidResult = given()
+                .header("Request-Id", bid)
+                .body(info)
+                .contentType(ContentType.JSON)
+                .when().post(String.format("/v2/platform/pkeys/%s/boxinfo", "4d27e0de-76b6-4f6b-871e-33c30b62ee48"))
+                .body()
+                .as(PollPkeyInvalidRsp.class);
+        Assertions.assertNotNull(invalidResult);
+        Assertions.assertEquals(String.format("SSP-%d", PKEY_INVALID.getCode()), invalidResult.getCode());
+        Assertions.assertEquals(PKEY_INVALID.getMessage(), invalidResult.getMessage());
+    }
+
+    @Test
+    public void pkeyPoll() {
+        final String bid = UUID.randomUUID().toString();
+        final GenPkeyRsp pkeyRsp = given()
+                .header("Request-Id", bid)
+                .contentType(ContentType.JSON)
+                .when().post("/v2/platform/pkeys")
+                .body()
+                .as(GenPkeyRsp.class);
+        Assertions.assertNotNull(pkeyRsp);
+
+        String pkey = pkeyRsp.getPkey();
+        final PollPkeyRsp result = given()
+                .header("Request-Id", bid)
+                .contentType(ContentType.JSON)
+                .when().get(String.format("/v2/platform/pkeys/%s/boxinfo", pkey))
+                .as(PollPkeyRsp.class);
+        Assertions.assertNotNull(result);
+
+        final PollPkeyInvalidRsp invalidResult = given()
+                .header("Request-Id", bid)
+                .contentType(ContentType.JSON)
+                .when().get(String.format("/v2/platform/pkeys/%s/boxinfo", "4d27e0de-76b6-4f6b-871e-33c30b62ee48"))
+                .body()
+                .as(PollPkeyInvalidRsp.class);
+        Assertions.assertNotNull(invalidResult);
+        Assertions.assertEquals(String.format("SSP-%d", PKEY_INVALID.getCode()), invalidResult.getCode());
+        Assertions.assertEquals(PKEY_INVALID.getMessage(), invalidResult.getMessage());
     }
 }
