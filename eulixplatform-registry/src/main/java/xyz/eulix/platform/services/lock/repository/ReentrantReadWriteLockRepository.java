246bfb0da0a2465102d84083e0142afe92c59dab
WeiWeiwei <wwweiv@nuaa.edu.cn>
2023-09-21 18:01:43 +0800

    fix():remove entityManager

--- eulixplatform-registry/src/main/java/xyz/eulix/platform/services/lock/repository/ReentrantReadWriteLockRepository.java
+++ eulixplatform-registry/src/main/java/xyz/eulix/platform/services/lock/repository/ReentrantReadWriteLockRepository.java
@@ -1,12 +1,9 @@
 package xyz.eulix.platform.services.lock.repository;
 
-
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import xyz.eulix.platform.services.lock.entity.ReentrantReadWriteLockEntity;
 
 import javax.enterprise.context.Dependent;
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
 
 /**
  * @author VvV
@@ -15,12 +12,7 @@ import javax.persistence.EntityManager;
 @Dependent
 public class ReentrantReadWriteLockRepository implements PanacheRepository<ReentrantReadWriteLockEntity> {
 
-
-    @Inject
-    EntityManager entityManager;
-
     public ReentrantReadWriteLockEntity findByLockKey(String lockKey) {
-        entityManager.clear();
         return this.find("lock_key", lockKey).firstResult();
     }
 
